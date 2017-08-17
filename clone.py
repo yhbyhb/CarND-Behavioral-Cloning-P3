ae2f7cf78ee3e222e5c1fc683cdb57493e1b32ca
HanByul Yang <hanbyul.yang@gmail.com>
2017-08-17 22:22:37 +0900

    Cropping images

--- clone.py
+++ clone.py
@@ -30,19 +30,17 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, 
-	input_shape=(160,320,3)))
-model.add(Convolution2D(6, 5, 5))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Activation('relu'))
-model.add(Convolution2D(16, 5, 5))
+model.add(Convolution2D(16, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Activation('relu'))
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Dense(84))
