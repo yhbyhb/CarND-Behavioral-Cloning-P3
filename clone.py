b76f147a366e2b421f543cb20423404a5f91da99
HanByul Yang <hanbyul.yang@gmail.com>
2017-08-17 22:03:43 +0900

    Applied LeNet.
        - 10. More Networks

--- clone.py
+++ clone.py
@@ -23,14 +23,26 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, 
+	input_shape=(160,320,3)))
+model.add(Convolution2D(6, 5, 5))
+model.add(MaxPooling2D())
+model.add(Activation('relu'))
+model.add(Convolution2D(16, 5, 5))
+model.add(MaxPooling2D())
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, 
-	shuffle=True, nb_epoch=7)
+	shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
