c72d4dde6ab34f02a626dc8dd9f471da2cef9ff2
HanByul Yang <hanbyul.yang@gmail.com>
2017-08-18 15:26:32 +0900

    generator

--- model.py
+++ model.py
@@ -1,33 +1,49 @@
 import csv
 import cv2
 import numpy as np
+import sklearn
+import os
+from sklearn.model_selection import train_test_split
 
 lines = []
-with open('./data/driving_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		lines.append(line)
+data_path = './data/'
+with open(os.path.join(data_path, 'driving_log.csv')) as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
 
-images = []
-measurements = []
-for line in lines:
-	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = './data/IMG/' + filename
-	image = cv2.imread(current_path)
-	images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-	augmented_images.append(image)
-	augmented_measurements.append(measurement)
-	augmented_images.append(cv2.flip(image, 1))
-	augmented_measurements.append(measurement * -1.0)
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        # shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = os.path.join(data_path, 'IMG/') + batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+                # # data augmentation
+                # images.append(cv2.flip(center_image, 1))
+                # angles.append(center_angle * -1.0)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
@@ -50,8 +66,9 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit(X_train, y_train, validation_split=0.2, 
-	shuffle=True, nb_epoch=5)
+history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
+                    validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples), nb_epoch=5)
 
 model.save('model.h5')
 
