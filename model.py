8bb32da81a30c30f81ab592dd9ab32116b6fd5d7
HanByul Yang <hanbyul.yang@gmail.com>
2017-08-21 15:06:27 +0900

    Refined comments of model.py

--- model.py
+++ model.py
@@ -12,20 +12,23 @@ with open(os.path.join(data_path, 'driving_log.csv')) as csvfile:
     for line in reader:
         lines.append(line)
 
+# Randomly split data into training and validation.
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
+# Generator function for fit_generator()
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
-
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
 
             images = []
             angles = []
             for batch_sample in batch_samples:
+                # Use center camera image only
                 name = os.path.join(data_path, 'IMG/') + batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
+                # Convert color channel format from BGR to RGB since drive.py uses RGB format.
                 center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
@@ -35,17 +38,19 @@ def generator(samples, batch_size=32):
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-
 train_generator = generator(train_samples)
 validation_generator = generator(validation_samples)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 
+# Model architecture
 model = Sequential()
+# Preprocessing - Crop some top and bottom pixels and normalization.
 model.add(Cropping2D(cropping=((65,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+# NVIDIA's architecture 
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
@@ -57,24 +62,25 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# ADAM optimizer and mean squared error for loss function.
 model.compile(loss='mse', optimizer='adam')
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
                     validation_data=validation_generator,
                     nb_val_samples=len(validation_samples), nb_epoch=5)
 
+# Save model for testing 
 model.save('model.h5')
 
-### print the keys contained in the history object
+# print the keys contained in the history object
 print(history_object.history.keys())
 
-### plot the training and validation loss for each epoch
+# plot the training and validation loss for each epoch
 import matplotlib.pyplot as plt
-
 plt.plot(history_object.history['loss'])
 plt.plot(history_object.history['val_loss'])
 plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
-# plt.show()
+# save plot
 plt.savefig('figure.png')
